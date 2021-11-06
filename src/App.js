9b6131d0b70e2d88f598953133b67ca14682dfd8
Ida Merete <idam.markussen@gmail.com>
2021-11-06 17:34:30 +0100

    Fixing css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import CalendarDisplay from './CalendarDisplay';
-import Navbar from './components/Navbar';
+import React from "react";
+import CalendarDisplay from "./CalendarDisplay";
+import Navbar from "./components/Navbar";
+import "./styles/App.css";
 
 function App() {
   return (
